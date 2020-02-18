1db01874b6789cfd6aaab8167d1268f4f8c604ba
Joel <jcosta8@boscdelacoma.cat>
2020-02-18 16:50:56 +0100

    Primera classe

---
+++ player/Player.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public class Player : Area2D
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	// Called when the node enters the scene tree for the first time.
+	[Export]
+	public int Speed = 400; // How fast the player will move (pixels/sec).
+
+	private Vector2 _screenSize; // Size of the game window.
+	public override void _Ready()
+	{
+		
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
